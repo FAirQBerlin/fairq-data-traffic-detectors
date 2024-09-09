d2179cf60104d97e39f0485a3c3513793dc9d6bd
Jenkins Daemon <brother-mfc@inwt-statistics.de>
2024-09-09 19:05:50 +0200

    Version: 24.09.0

--- R/02_urls.R
+++ R/02_urls.R
@@ -8,7 +8,7 @@ detector_meta_url <- function() {
 }
 
 
-create_urls <- function(max_month = list(year = 2024, month = 3)) {
+create_urls <- function(max_month = list(year = 2024, month = 7)) {
   max_year <- max_month$year
   min_year <- 2015
   years <- seq(min_year, max_year)
--- tests/testthat/test-01_observations.R
+++ tests/testthat/test-01_observations.R
@@ -15,7 +15,7 @@ test_that("create_urls() creates correct URLS depending on month and year", {
     res[length(res)],
     paste0(
       "https://mdhopendata.blob.core.windows.net/verkehrsdetektion/2024/2024/Messquerschnitte%20",
-      "(richtungsbezogen)/mq_hr_2024_03.csv.gz"
+      "(richtungsbezogen)/mq_hr_2024_07.csv.gz"
     )
   )
 })
